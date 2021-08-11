83b5770050c0c44fe81677112ef68f60c16cc61f
Philip Sampaio <philip.sampaio@gmail.com>
2021-08-11 02:20:32 -0300

    Fish and nvim related config

--- fish/common.fish
+++ fish/common.fish
@@ -1,8 +1,10 @@
 set -g fish_user_paths "$HOME/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
 
 set -x GPG_TTY (tty)
 set -x EDITOR nvim
+set -x ECTO_EDITOR nvim
 
 if status --is-interactive
   set -g fish_user_abbreviations
