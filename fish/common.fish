7aa799a780e9b0066a35d8f93c2f4cbeb31caea8
Philip Sampaio <philip.sampaio@gmail.com>
2022-02-04 21:24:50 -0300

    Add FLYCTL_INSTALL env var for fish

--- fish/common.fish
+++ fish/common.fish
@@ -5,6 +5,7 @@ set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
 set -x GPG_TTY (tty)
 set -x EDITOR nvim
 set -x ECTO_EDITOR nvim
+set -x FLYCTL_INSTALL "/home/philip/.fly"
 
 if status --is-interactive
   set -g fish_user_abbreviations
