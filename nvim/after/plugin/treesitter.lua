266c85052e97958900ba3721d9962a5424504fd2
Philip Sampaio <philip.sampaio@gmail.com>
2023-05-19 02:09:36 -0300

    Keep the same style

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup({
   -- A list of parser names, or "all"
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "rust" },
 
@@ -18,4 +18,4 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-}
+})
