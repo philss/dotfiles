199ee5f28f41efa9526e1ee3ec538945513d67c5
Philip Sampaio <philip.sampaio@gmail.com>
2023-11-22 15:06:27 -0300

    Simplify

--- nvim/after/plugin/editor-config.lua
+++ nvim/after/plugin/editor-config.lua
@@ -2,7 +2,7 @@
 require('editorconfig').properties.max_line_length = function(bufnr, val, opts)
   local ft = vim.bo.filetype
 
-  if val and ft and ft == "elixir" then
+  if val and ft == "elixir" then
     -- remove text-width, and therefore the automatic wrapping.
     vim.bo.textwidth = 0
     -- set the column to this "max_line_length" position instead.
