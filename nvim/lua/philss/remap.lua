a301494bd54489813e9505f109bd6f490fb5ce21
Philip Sampaio <philip.sampaio@gmail.com>
2024-08-19 01:10:55 -0300

    Remove mapping of J and K to move lines around

--- nvim/lua/philss/remap.lua
+++ nvim/lua/philss/remap.lua
@@ -3,7 +3,3 @@ vim.keymap.set("n", "<tab>", ":tabnext<CR>")
 
 -- Mapping control + <tab> to go to the previous tab
 vim.keymap.set("n", "<S-tab>", ":tabprevious<CR>")
-
--- Move the text under selection to +1 or -2 lines
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
