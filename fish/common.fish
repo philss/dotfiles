02e64b1c0b32c24121570dffb2f85ccfe045c154
Philip Sampaio <philip.sampaio@gmail.com>
2026-02-26 18:48:42 -0300

    Install catppuccin tmux theme and fix/cleanup configs
    This is changing the "base" file for the fish config, and also it's
    fixing instructions at the "scripts/doctor" for the new tmux theme and
    changes to the "PATH" env var.

--- fish/common.fish
+++ fish/common.fish
@@ -1,21 +1,36 @@
-set -g fish_user_paths "$HOME/bin" $fish_user_paths
-set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
-set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
-set -g fish_user_paths "$HOME/.cache/rebar3/bin" $fish_user_paths
+# A base for fish configuration
 
-set -x GPG_TTY (tty)
+# On Linux this might be necessary.
+# set -x GPG_TTY (tty)
 set -x EDITOR nvim
-set -x ECTO_EDITOR nano
-set -x FLYCTL_INSTALL "/home/philip/.fly"
 
-if status --is-interactive
+if status is-interactive
   set -g fish_user_abbreviations
   abbr -a vim nvim
   abbr -a tx env TERM=screen-256color tmuxinator
   abbr -a gco git checkout
-  abbr -a format "git status --porcelain | cut -d' ' -f3 | xargs | mix format"
-  abbr -a ls exa
-  abbr -a tree "exa -T"
+  abbr -a ls eza
+  abbr -a tree "eza -T"
 end
 
-source ~/.asdf/asdf.fish
+# Local bins
+set _local_bin "$HOME/.local/bin"
+if not contains $_local_bin $PATH
+  mkdir -p $_local_bin
+  set -gx --prepend PATH $_local_bin
+end
+set --erase _local_bin
+
+# ASDF configuration code (version  ~> 0.18.0)
+if test -z $ASDF_DATA_DIR
+  set _asdf_shims "$HOME/.asdf/shims"
+else
+  set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+  set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
